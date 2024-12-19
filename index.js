7c9bd77b204352cab50c0c3213aa23fe553f1afb
yeseongMAP <yesmapsule0221@gmail.com>
2024-12-19 16:34:07 +0900

    feat: morgan middleware 기본안 구현

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const morganMiddleware = require('./src/middleware/morganMiddleware');
+
 const app = express();
 require('dotenv').config();
+
+// app.use(morgan('dev')); //개발환경 로그
+// app.use(morgan('short')); //응답시간 포함
+
+app.use(morganMiddleware);
+
+console.log('인입환경변수 확인:', process.env.NODE_ENV);
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 app.listen(process.env.mainPort, function () {
   console.log('서버열림');
