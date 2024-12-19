7f005451ee1d1bd0b60b3515a19043c782463f02
yeseongMAP <yesmapsule0221@gmail.com>
2024-12-19 14:30:00 +0900

    docs: morgan 추가

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-console.log(process.env.mainPort);
-
-console.log('확인:', process.env.NODE_ENV);
-
-app.listen(9999, function () {
+app.listen(process.env.mainPort, function () {
   console.log('서버열림');
 });
