628ac17c99b8614da7ddff0a6d9c1bdbbb86e095
yeseongMAP <yesmapsule0221@gmail.com>
2024-12-23 16:51:26 +0900

    feat: err middlewear 기본안

--- src/mainRouter.js
+++ src/mainRouter.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
   res.status(200).json({ msg: '기본라우팅 설정' });
 });
 
+//err middleware
+router.use((err, req, res, next) => {
+  if (err.message === 'typeError') {
+    res.status(400).json({ message: 'something is wrong' });
+  }
+  return;
+});
+
 module.exports = router;
